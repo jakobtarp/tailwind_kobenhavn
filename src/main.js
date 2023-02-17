58605880e96ebdab8f96f32f5d7446687b966ce0
jakobtarp <jakob.tarp@hotmail.com>
2023-02-17 12:57:48 +0100

    Done_final

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import "./style.css";
+import { darkMode } from "./darkmode";
+
+darkMode();
+
+/* Import af Motion One bibliotek */
+import { animate, stagger, inView, scroll, timeline } from "motion";
+
+animate("body", { opacity: [0, 1] }, { duration: 5 });
+scroll(animate(".progress-bar", { scaleX: [0, 1] }));
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
+    colors: {
+      blue: {
+        300: "#93c5fd",
+      },
+      violet: {
+        100: "#ede9fe",
+      },
+      purple: {
+        100: "#f3e8ff",
+      },
+      white: {
+        white: "#ffffff",
+      },
+      black: {
+        black: "#000",
+      },
+      a: {
+        dark: "#181818",
+      },
+      b: {
+        lessdark: "#282828",
+      },
+      c: {
+        light: "#404040",
+      },
+    },
     extend: {
-      colors: {
-        grayish: {
-          100: "#CCCCCC",
-        },
-        purpleish: {
-          100: "#2243a8",
-        },
+      fontFamily: {
+        typo: "Poppins, sans-serif",
       },
       plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
     },
